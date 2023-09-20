54ed9d70028ef6382b17663e20816778131f6861
Carter <carter@pangweb.com>
2023-09-19 22:16:36 -0700

    added projects

--- src/pages/PortfolioPg.jsx
+++ src/pages/PortfolioPg.jsx
@@ -1,8 +1,27 @@
 export default function PortfolioPage() {
     return (
+     
+        <div>
         <div className="container pt-3">
-        DoggyGPS
-        </div>
+        DoggyGPS: 
 
+        <a href="https://github.com/funkycba/doggy-gps">github page</a>
+        </div>
+        <img href="#*" src="https://raw.githubusercontent.com/funkycba/doggy-gps/main/assets/images/doggygps-screenshot.png"></img>
+        <div className="container pt-3">
+        mvmnt: 
+
+        <a href="https://github.com/mslzbry/mvmnt">github page</a>
+        </div>
+        <img href="#*" src="https://github.com/mslzbry/mvmnt/raw/main/gif/mvmnt.gif"></img>
+                <div className="container pt-3">
+        Scorsteezy: 
+
+        <a href="https://github.com/funkycba/scoresteezy">github page</a>
+        </div>
+        <aside>
+            <img src="https://th.bing.com/th/id/OIP.x9GmbOiLHtLVgNnIwJmmggHaEG?w=302&h=180&c=7&r=0&o=5&pid=1.7"></img>
+            </aside>
+        </div>
     )
 }
